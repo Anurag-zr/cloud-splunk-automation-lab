3bb0618eb2085adf1931f779b41628ee35f11890
anurag <anuragpandit271@gmail.com>
2025-11-01 10:08:29 +0530

    added cloudtrails output names in root outputs.tf file

---
+++ Terraform-IaC/aws/outputs.tf
@@ -0,0 +1,11 @@
+output "cloudtrail_bucket_name" {
+  value = module.cloudtrail.cloudtrail_bucket_name
+}
+output "cloudtrail_bucket_arn" {
+  value = module.cloudtrail.cloudtrail_bucket_arn
+}
+
+output "cloudtrail_name" {
+    value = module.cloudtrail.cloudtrail_name
+}
+
