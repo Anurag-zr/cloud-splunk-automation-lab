493c4fdd7e40448ec09f17be1307bcf9227b441e
anurag <anuragpandit271@gmail.com>
2025-11-02 10:39:16 +0530

    corrected guardDuty variable in main.tf to guardduty

--- Terraform-IaC/aws/main.tf
+++ Terraform-IaC/aws/main.tf
@@ -16,5 +16,5 @@ module "cloudtrail" {
 module "guardDuty" {
   count = var.enable_guardDuty?1:0
   source = "./modules/guardDuty"  
-  guardDuty_s3_bucket_name = "guardDuty-logs-${random_id.suffix.hex}"
+  guardDuty_s3_bucket_name = "guardduty-logs-${random_id.suffix.hex}"
 }
